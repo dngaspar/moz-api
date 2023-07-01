948beb6b19483226e12a7cf03218148d48ab99d3
Denis <dngaspar5@gmail.com>
2023-07-01 04:10:34 +0300

    Init api with Fast API

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# Use an official Python runtime as the base image
+FROM python:3.9-slim
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the requirements file and install dependencies
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the rest of the project code
+COPY . .
+
+# Expose the port on which your FastAPI application runs
+EXPOSE 8000
+
+# Define the command to run your application
+CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8000"]
---
+++ app.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+
+from models import Data
+
+description = """
+API description
+"""
+
+
+app = FastAPI(
+    title="Project Name",
+    description=description,
+    version="0.0.1",
+    contact={
+        "name": "Author",
+        "url": "https://example.com",
+        "email": "admin@example.com",
+    },
+)
+
+
+@app.get("/")
+def handle_get(id: int, name: str):
+    return f"GET: {id} - {name}"
+
+
+@app.post("/")
+def handle_post(data: Data):
+
+    id = data.id
+    name = data.name
+
+    return f"POST: {id} - {name}"
